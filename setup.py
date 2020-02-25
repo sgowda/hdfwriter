5a569635f3f0e62a6e5ce0494c1da5cf33b6fa73
Suraj Gowda <sgowda@github>
2020-02-25 10:35:26 -0800

    simplifying creation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'hdfwriter',         # How you named your package folder (MyLib)
   packages = ['hdfwriter'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Stream data to an HDF5 File',   # Give a short description about your library
   author = 'James Gao, Helene Moorman, Suraj Gowda',                   # Type in your name
